50efaf1906cd2e2efda39bb99491062ea5412ed2
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-04-30 11:26:06 +0600

    json cache no store added"

--- src/components/HomePage/PopularCourses.jsx
+++ src/components/HomePage/PopularCourses.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { cache } from 'react';
 import CourseCard from '../CourseCard';
 
 const PopularCourses =async () => {
-    const res = await fetch("https://a8-skill-sphere-online-learning-pla.vercel.app/courses.json");
+    const res = await fetch("https://a8-skill-sphere-online-learning-pla.vercel.app/courses.json",
+        {cache: 'no-store'});
     const courseData = await res.json();
    
     const popularCourse = courseData.sort((a,b)=> b.rating - a.rating).slice(0,3);
